d53345e70046d3f283b42fcd015bfc6f2c049d23
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-02-11 23:16:48 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+console.log(body);
+console.log(buttonStart);
+console.log(buttonStop);
+let timerId = null;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', (event) => {
+    buttonStart.disabled = !event.target.value;
+    buttonStop.disabled = event.target.value;
+    timerId = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`;
+        console.log('1321321');
+    }, 1000);
+
+});
+
+buttonStop.addEventListener('click', (event) => {
+    clearInterval(timerId);
+    buttonStart.disabled = event.target.value;
+});
 
